87cdc2e5d930a8a29294995d713170bed2a45051
Artemyze <hejiw@mail.ru>
2022-03-17 00:15:36 +0700

    try to do

--- news/newsportal/models.py
+++ news/newsportal/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
 
-from models_resources.source import CatSource
+from .models_resources.source import CatSource
 
 
 class Author(models.Model):
--- news/newsportal/views.py
+++ news/newsportal/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'post_list'
+
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'post_detail.html'  # название шаблона будет post_detail.html
+    context_object_name = 'post_detail'
